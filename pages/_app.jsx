f4fd612896152b8ed295ad8e786c705085c8f0c0
manuel <hbiaser132@gmail.com>
2021-08-16 16:49:55 -0500

    first commit

---
+++ pages/_app.jsx
@@ -0,0 +1,7 @@
+import  "../style/normalize.css";
+import "../style/global.css"
+
+export default function App({Component, pageProps}) {
+
+    return <Component {...pageProps}/> 
+}
---
+++ pages/posts/[id].jsx
@@ -0,0 +1,30 @@
+import { getAllPostId, getPostData } from "../../lib/post";
+import Layout from '../../components/layout'
+
+export async function getStaticPaths(){
+    const paths = getAllPostId()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}){
+    const postData = getPostData(params.id)
+
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+export default function Post({postData}) {
+    return <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+    </Layout>
+}
